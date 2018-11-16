8b9c993eb24f1d73626b4862d2965a05bd180fb1
Nydia <nyd.bubble@gmail.com>
2018-11-16 12:08:14 +0800

    Added user input

---
+++ src/com/nydia/GUI.java
@@ -0,0 +1,81 @@
+package com.nydia;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class GUI {
+
+    private JFrame frame;
+    private JTextField textField;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GUI window = new GUI();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public GUI() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        Button Submit = new Button("Submit");
+        Submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                String userInput = textField.getText();
+                System.out.println(userInput);
+//                QEPResults qep = new QEPResults();
+                Visualization.initQEP(userInput);
+                //qep.setVisible(true);
+
+            }
+        });
+        Submit.setBounds(54, 229, 70, 22);
+        frame.getContentPane().add(Submit);
+
+        Button Clear = new Button("Clear");
+        Clear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("");
+            }
+        });
+        Clear.setBounds(167, 229, 70, 22);
+        frame.getContentPane().add(Clear);
+
+        textField = new JTextField();
+        textField.setBounds(34, 42, 360, 178);
+        frame.getContentPane().add(textField);
+        textField.setColumns(10);
+
+        JLabel lblNewLabel = new JLabel("Enter SQL Query: ");
+        lblNewLabel.setBounds(34, 17, 142, 14);
+        frame.getContentPane().add(lblNewLabel);
+    }
+}
--- src/com/nydia/Visualization.java
+++ src/com/nydia/Visualization.java
@@ -29,6 +29,7 @@ public class Visualization extends JPanel {
     private static final int windowHeight = screenSize.height;
 
     private static QEP qep;
+    private int scaleX;
 
     public static void main(String[] args) {
         String userInput = "";
@@ -82,6 +83,42 @@ public class Visualization extends JPanel {
        //new Visualization(qep);
     }
 
+    public static void initQEP(String userInput) {
+        DbUtil.getConnection();
+        DbRepository dbRepository = new DbRepository();
+
+        //Query to database
+        Response r = dbRepository.getResponse(userInput);
+        QEP qep = buildQEP(r, userInput);
+
+        //Visualization
+        Visualization panel = new Visualization(qep);
+        panel.setPreferredSize(new Dimension(panel.calculateDimensions(), windowHeight));
+        System.out.println(panel.calculateDimensions());
+        JFrame window = new JFrame();
+
+        window.setLocationRelativeTo(null);
+        JScrollPane jsp = new JScrollPane(panel);
+        jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jsp.setBounds(0, 0, windowWidth, windowHeight-100);
+        JPanel contentPane = new JPanel(null);
+        contentPane.setPreferredSize(new Dimension(windowWidth, windowHeight-100));
+        contentPane.add(jsp);
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setContentPane(contentPane);
+        //window.setContentPane(new Visualization(qep));
+        //window.add(jsp);
+        //window.add(panel);
+        window.pack();
+
+        window.revalidate();
+        window.repaint();
+        window.setVisible(true);
+    }
+
     public static QEP buildQEP(Response r, String query) {
         //Build the QEP tree
         Node root = new Node(r.getPlan());
@@ -102,19 +139,43 @@ public class Visualization extends JPanel {
     private static int tambahY;
     private static int tambahX;
 
+    public int calculateDimensions() {
+        int result = windowWidth;
+        Node root = qep.getRoot();
+        int n = qep.getLevel(root, 0);
+        int temp = (int) Math.pow(2, n)-1;
+        System.out.println(n);
+        System.out.println(temp);
+        tambahY = (windowHeight-50)/(n);
+        tambahX = windowWidth/temp;
+        while (tambahX < 80) {
+            result += 160;
+            System.out.println(result);
+            tambahX = result/temp;
+        }
+        tambahX = tambahX*n;
+        return result;
+    }
+
     @Override
     public void paint(Graphics graphics) {
+        Dimension dim = getSize ();
         graphics.setColor(Color.LIGHT_GRAY);
         //graphics.fillRect(0, 0, SCALE_X * WIDTH, SCALE_Y * HEIGHT);
 
         Node root = qep.getRoot();
         int n = qep.getLevel(root, 0);
-        System.out.println(n);
-        int temp = n*(n+1)/2;
-        tambahY = 1000/(temp);
-        tambahX = 160;
-        System.out.println(tambahX);
-        paintNode(graphics, root, (windowWidth)/2, 50, n);
+        int temp = (int) Math.pow(2, n)-1;
+        tambahY = (dim.height-50)/(n);
+        /*while (tambahX < 160) {
+            dim.setSize(dim.width + 160, dim.height);
+            dim = getSize ();
+            System.out.println(dim.width);
+            tambahX = dim.width/temp;
+            System.out.println(tambahX);
+        }*/
+
+        paintNode(graphics, root, dim.width/2, 50, tambahX);
         //paintNode(graphics, root, n);
     }
 
@@ -137,9 +198,7 @@ public class Visualization extends JPanel {
     private void paintNode(Graphics g, Node root, int x, int y, int level) {
         if (root == null)
             return;
-        if (level <= 0) {
-            System.out.println("negative");
-        }
+
         g.setColor(Color.black);
 
         String text = root.getType();
@@ -160,19 +219,19 @@ public class Visualization extends JPanel {
 
         if (root.getRightChild() != null) {
             g.setColor(Color.black);
-            g.drawLine(x, y + ovalHeight/2, x - (tambahX), y+(level+1)*tambahY +ovalHeight/2);
-            paintNode(g, root.getLeftChild(), x - (tambahX), y+(level+1)*tambahY, level-1);
+            g.drawLine(x, y + ovalHeight/2, x - (level), y+tambahY +ovalHeight/2);
+            paintNode(g, root.getLeftChild(), x - (level), y+tambahY, tambahX/2);
 
             //System.out.println(level);
             g.setColor(Color.black);
-            g.drawLine(x, y + ovalHeight/2, x + tambahX, y+(level+1)*tambahY +ovalHeight/2);
-            paintNode(g, root.getRightChild(), x + tambahX, y+(level+1)*tambahY, level-1);
+            g.drawLine(x, y + ovalHeight/2, x + level, y+tambahY +ovalHeight/2);
+            paintNode(g, root.getRightChild(), x + level, y+tambahY, tambahX/2);
         }
         //Draw straight
         else if (root.getLeftChild() != null) {
             g.setColor(Color.black);
-            g.drawLine(x, y + ovalHeight/2, x, y+(level+1)*tambahY +ovalHeight/2);
-            paintNode(g, root.getLeftChild(), x, y+(level+1)*tambahY, level);
+            g.drawLine(x, y + ovalHeight/2, x, y+tambahY +ovalHeight/2);
+            paintNode(g, root.getLeftChild(), x, y+tambahY, level);
 
         }
     }
